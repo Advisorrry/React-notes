fbce1dc46c2cd8cda7d1bab867eab883d96dceb7
Advisorrry <sypikeh@yandex.com>
2020-10-18 14:31:08 +0500

    New Project

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => (
+    <div className="jumbotron ">
+        <div className="container">
+            <h1 className="display-4">React-приложение</h1>
+            <p className="lead">Создано с использование React, Bootstrap и SCSS.</p>
+        </div>
+    </div>
+)
